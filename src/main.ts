d724c4a278f32dc1362cd9dbc88ef32500d4e913
cyokozai <yuu4suke27@outlook.jp>
2025-11-17 11:37:21 +0000

    main.tsにgreet関数を追加し、コンソールに挨拶メッセージを表示するコードを実装

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// import { log } from 'console';
+// import mineflayer from 'mineflayer';
+
+console.log(greeting);
+
+function greet(name: string): string {
+  return `こんにちは、${name}さん！`;
+}
+
+console.log(greet("太郎"));
